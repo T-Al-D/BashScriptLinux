c3775a5e0b5b224048a1cc684d93c2e01f0743c8
T-Al-D <tahaaldailami2010@hotmail.de>
2023-07-22 10:30:30 +0200

    Update bashScript1.sh
    added to the script

--- bashScript1.sh
+++ bashScript1.sh
@@ -2,32 +2,56 @@
 
 # variables
 NOW=$(date)
+USER=$(whoami)
+
+# set BASH to quit if error arises
+set -e
 
 # logs can be used to retrace what happend
 echo "log START $NOW :" >> Logs.txt
 
 # -le= True if equal or less than,    -eq= Only true if equal
 # -ge= True if equal or greater than
-# $# = numer of parameters given
+# $# = numer of parameters given ,     >> is appending text to a file
 
 if [ $# -eq 0 ]; then
-	echo "if"
-
+	# update + upgrade system
+	sudo apt update
+	sudo apt dist-upgrade
+	clear
+	echo "system updated" >> Logs.txt
 elif [ $1 -le 1 ]; then
-	echo "elif1"
-
+	# output systeminformation
+	echo "currently logged in as $USER."
+	echo "Memory: "
+	free -h
+	echo "Disk Usage: "
+	df -h
+	echo "Uptime :"
+	uptime -p
+	echo "systeminformation " >> Logs.txt
 elif [ $1 -eq 2 ]; then
-	echo "elif2"
-
-else [ $1 -ge 3 ]
-	echo "else"
-
+	# create multiple directories with multiple files inside
+	for VAR in {1..3}; do
+	mkdir -p dir$VAR
+	cd dir$VAR
+	touch file{1..3}.txt
+	cd ..
+	done
+	echo "directories and files created"
+	echo "create multiple dirctoies and files" >> Logs.txt
+elif [ $1 -eq 3 ]; then
+	echo "Logs.txt words:"
+	wc -w < Logs.txt
+	echo "Logs.txt lines:"
+	wc -l < Logs.txt
+	echo "Information about LOgs.txt" >> Logs.txt
+else [ $1 -ge 4 ]
+	echo "nothing has been done" >> Logs.txt
 fi
 
 
 
 echo "log END." >> Logs.txt
 echo "script executed successfully!"
-
 exit 0
-# exit 0 means that the programm/script ended in success
